906f16b3a9398d02011d80d3ac8386fcbef2c2b0
qisensen <senssic@foxmail.com>
2017-08-08 17:46:57 +0800

    优化传入项

--- src/main/java/com/sc/science/utils/FindClassInPath.java
+++ src/main/java/com/sc/science/utils/FindClassInPath.java
@@ -3,25 +3,24 @@ package com.sc.science.utils;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
-import java.util.zip.ZipEntry;
 
 /**
  * 指定路径查找类文件
  */
 public class FindClassInPath {
     private static String CLASS_FILE_TO_FIND =
-            "com.google.common.collect.ImmutableSet";//形式  com/google/common/collect/ImmutableSet.class or com.google.common.collect.ImmutableSet
+            "com/google/common/collect/ImmutableSet";//形式  com/google/common/collect/ImmutableSet.class or com.google.common.collect.ImmutableSet
     private static String FILE_TO_PATH = "C:\\Users\\Administrator\\Desktop\\prelog\\lib";
     private static List<String> foundIn = new LinkedList<String>();
     private static List<String> foundNotIn = new LinkedList<String>();
 
     public static void main(String[] args) {
-        if (!CLASS_FILE_TO_FIND.endsWith(".class")) {
-            CLASS_FILE_TO_FIND = CLASS_FILE_TO_FIND.replace('.', '/') + ".class";
-        }
+        CLASS_FILE_TO_FIND = CLASS_FILE_TO_FIND.replace('.', '/');
         if (args.length > 1) {
             CLASS_FILE_TO_FIND = args[1];
             FILE_TO_PATH = args[0];
@@ -63,14 +62,12 @@ public class FindClassInPath {
             System.out.println("Searching: " + f.getPath());
             foundNotIn.add(f.getPath());
             JarFile jar = new JarFile(f);
-            ZipEntry e = jar.getEntry(CLASS_FILE_TO_FIND);
-            if (e == null) {
-                e = jar.getJarEntry(CLASS_FILE_TO_FIND);
-                if (e != null) {
-                    foundIn.add(f.getPath());
+            Enumeration<JarEntry> entryEnumeration=jar.entries();
+            while (entryEnumeration.hasMoreElements()) {
+                JarEntry jarEntry= entryEnumeration.nextElement();
+                if (jarEntry.getName().contains(CLASS_FILE_TO_FIND)) {
+                    foundIn.add(f.getPath()+":"+jarEntry.getName());
                 }
-            } else {
-                foundIn.add(f.getPath());
             }
         } catch (IOException e) {
             e.printStackTrace();
