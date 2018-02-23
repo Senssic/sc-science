c20e8945ac7a4638b43fbaf5255f90f10bde47c5
qisensen <senssic@foxmail.com>
2018-02-23 17:27:19 +0800

    设计模式分类

---
+++ src/main/java/com/sc/science/utils/MapUtils.java
@@ -0,0 +1,21 @@
+package com.sc.science.utils;
+
+import java.util.Map;
+
+/**
+ * <一句话功能简述>
+ * <功能详细描述>
+ *
+ * @auth:qiss
+ * @see: [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+public class MapUtils {
+    public static boolean isEmpty(Map<?, ?> map) {
+        return map == null || map.isEmpty();
+    }
+
+    public static boolean isNotEmpty(Map<?, ?> map) {
+        return !isEmpty(map);
+    }
+}
